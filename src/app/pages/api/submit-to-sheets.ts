4085447544dc006ca2fcf7162044e1e98e76a302
DOC-weed <aleesfeo1234@gmail.com>
2025-02-19 14:00:19 -0800

    wip

--- src/app/pages/api/submit-to-sheets.ts
+++ src/app/pages/api/submit-to-sheets.ts
@@ -3,9 +3,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { google } from 'googleapis';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    return res.status(405).end(); // Solo se permite POST
-  }
+
 
   const { rows } = req.body;
 
